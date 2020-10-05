29b93c077c192ab5d37d3ae7637dd884268a2623
kiyoung85 <kiyoung85@gmail.com>
2020-10-06 01:16:20 +0900

    JPQL - 엔티티 직접 사용

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -50,33 +50,13 @@ public class JpaMain {
             em.clear();
 
 //            String query = "select m from Member m";
-            String query = "select m from Member m join fetch m.team";
-            List<Member> resultList = em.createQuery(query, Member.class).getResultList();
-
-            for (Member m : resultList) {
-                System.out.println("member3 = " + m.getUsername() + ", " + m.getTeam().getName());
-                // 회원1, 팀A(SQL)
-                // 회원2, 팀A(1 차 캐시)
-                // 회원3, 팀B(SQL)
-
-                // 회원 100명 -> 쿼리가 100 번 나감: N + 1
-                
-                // fetch 조인으로 변경 하면 select 쿼리 한번에 모든 정보를 다 가져올 수 있다
-                // 지연 로딩 없이 그냥 한번에 다 가져온다
-            }
-
-            query = "select distinct t From Team t join fetch t.members m";
-            List<Team> resultList1 = em.createQuery(query, Team.class)
+            String query = "select m from Member m where m = :member";
+            List<Member> resultList = em.createQuery(query, Member.class)
+                    .setParameter("member", member1)
                     .getResultList();
 
-            for (Team team : resultList1) {
-                System.out.println("team = " + team.getName() + " , " + team.getMembers().size());
-                for(Member mem : team.getMembers()) {
-                    System.out.println("mem = " + mem);
-                }
-                //team = 팀A , 2
-                //team = 팀A , 2 -> 왜 2개지? 일 대 다 관계에서는 데이터가 늘어 날 수 밖에 없다
-                //team = 팀B , 1
+            for (Member member3 : resultList) {
+                System.out.println("m = " + member3);
             }
 
 
