5d18bac4987e77e0b6310f03d15cd803afe78b3c
kiyoung85 <kiyoung85@gmail.com>
2020-10-05 16:54:46 +0900

    JPQL - 경로 표현식

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -30,25 +31,14 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            // de 가 어느 위치에 있니?, 4번째 그래서 4를 반환 함
-            String query = "select locate('de', 'abcdefg') from Member m";
+//            String query = "select m.username From Member m";
+//            String query = "select t.members From Team t";
+            String query = "select m.username From Team t join t.members m";
             List<Integer> resultList = em.createQuery(query, Integer.class)
                     .getResultList();
 
             for (Integer integer : resultList) {
                 System.out.println("integer = " + integer);
-            }
-
-            // Team 엔티티 안에 members 는 List 이다. size 함수는 해당 List 의 size 를 반환 한다.
-            query = "select size(t.members) from Team t";
-            em.createQuery(query);
-
-            query = "select function('group_concat', m.username) from Member m";
-            List<String> resultList1 = em.createQuery(query, String.class)
-                    .getResultList();
-
-            for (String s : resultList1) {
-                System.out.println("s = " + s);
             }
 
             tx.commit();
